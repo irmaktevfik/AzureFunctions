2a211da9bee96530814df1c88ef381654595952f
irmaktevfik <irmak.tevfik@outlook.com>
2016-11-24 12:09:45 +0300

    +done with signal r

---
+++ CommentAnalytics/CommentAnalytics.Services/CommentHub.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Newtonsoft.Json;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace CommentAnalytics.Services
+{
+    [HubName("CommentHub")]
+    public class CommentHub : Hub
+    {
+        public void PostComment(string comment, int score)
+        {
+            var commentScore = new CommentsModel() { Percentage = score, Comment = comment };
+            Clients.All.commentShow(Newtonsoft.Json.JsonConvert.SerializeObject(commentScore));
+        }
+    }
+
+    public class CommentsModel
+    {
+        [JsonProperty(PropertyName = "percentage")]
+        public int Percentage { get; set; }
+
+        [JsonProperty(PropertyName = "comment")]
+        public string Comment { get; set; }
+    }
+}
